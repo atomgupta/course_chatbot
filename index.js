0118772df32678becf8af3a3e2c6ed30963086d0
unknown <pushpakguptacsc1@gmail.com>
2020-01-08 14:05:36 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+    res.send({"hello":"there"})
+})
+app.listen(process.env.PORT||3000,()=>{
+    console.log('server running');
+})
